1c5a432430438f81ba478661547af50034672b43
张金阳 <523213189@qq.com>
2016-12-01 22:22:22 +0800

    ver1.0

---
+++ app/src/main/java/com/npu/zhang/npulibrary/DetailActivity.java
@@ -0,0 +1,30 @@
+package com.npu.zhang.npulibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        String title = intent.getStringExtra("bookNameReal");
+        setTitle(title);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        //webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        webView.loadUrl(url);
+
+    }
+}
--- app/src/main/java/com/npu/zhang/npulibrary/MainActivity.java
+++ app/src/main/java/com/npu/zhang/npulibrary/MainActivity.java
@@ -1,5 +1,6 @@
 package com.npu.zhang.npulibrary;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.ScrollingTabContainerView;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -48,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
         editText = (EditText) findViewById(R.id.editText2);
-        textView = (TextView) findViewById(R.id.textView2);
         listView = (ListView) findViewById(R.id.listView);
 
         findViewById(R.id.btnSearch).setOnClickListener(new View.OnClickListener() {
@@ -56,43 +57,84 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 progressBar.setVisibility(View.VISIBLE);
                 String book = editText.getText().toString();
+                if (book.equals(null)){
+                    return;
+                }
+                String page = "1";
+                AsyncTask(book,page);
 
-                new AsyncTask<String, Void, List<Map<String, String>>>(){
-                    @Override
-                    protected List<Map<String, String>> doInBackground(String... strings) {
-                        try {
-                            System.out.println("Begin to connnet!");
-                            Document document = Jsoup.parse(new URL("http://202.117.255.187:8080/opac/openlink.php?strSearchType=title&strText=" + strings[0] + "&page=1"), 5000);
-                            System.out.println("Connect successful!");
-                            Element link = document.select("ol").first();
-                            Elements atags = link.select("a");
-                            Elements h3Tags = link.select("h3");
-                            List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-                            for (Element h3Tag : h3Tags){
-                                if (h3Tag.text().equals("馆藏"))
-                                    continue;
-                                System.out.println("书名：" + h3Tag.text().substring(4));
-                                System.out.println("    链接：" + h3Tag.select("a").attr("href"));
-                                Map<String, String> map = new HashMap<String, String>();
-                                map.put("Name", h3Tag.text().substring(4));
-                                map.put("href","http://202.117.255.187:8080/opac/" + h3Tag.select("a").attr("href"));
-                                list.add(map);
-                            }
-                            return list;
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-                    @Override
-                    protected void onPostExecute(List<Map<String, String>> list) {
-                        listView.setAdapter(new SimpleAdapter(MainActivity.this, list, android.R.layout.simple_list_item_2,
-                                new String[] {"Name","href"}, new int[] {android.R.id.text1, android.R.id.text2}));
-                        progressBar.setVisibility(View.GONE);
-                        super.onPostExecute(list);
-                    }
-                }.execute(book);
             }
         });
+
+        ((ListView) findViewById(R.id.listView)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ListView listView = (ListView) adapterView;
+                HashMap<String, String> map = (HashMap<String, String>) listView.getItemAtPosition((int) l);
+                String link = map.get("bookLink");
+                Intent intent = new Intent(MainActivity.this,DetailActivity.class).putExtra("url", link);
+                intent.putExtra("bookNameReal", map.get("bookNameReal"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    void AsyncTask(String book, String page){
+        new AsyncTask<String, Void, List<Map<String, String>>>(){
+            @Override
+            protected List<Map<String, String>> doInBackground(String... strings) {
+                try {
+                    System.out.println("Begin to connnet!");
+                    Document document = Jsoup.parse(new URL("http://202.117.255.187:8080/opac/openlink.php?strSearchType=title&strText=" + strings[0] + "&page=" + strings[1]), 5000);
+                    System.out.println("Connect successful!");
+                    Element link = document.select("ol").first();
+                    if (link == null){
+                        System.out.println("NullPointer!");
+                        return null;
+                    }
+
+                    Elements liTags = link.select("li");
+                    List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+                    for (Element liTag : liTags){
+                        Element h3Tag = liTag.select("h3").first();
+                        Element aTag = liTag.select("a").first();
+                        Element pTag = liTag.select("p").first();
+                        if (h3Tag.text().equals("馆藏"))
+                            continue;
+                        String bookName = h3Tag.text().substring(4);
+                        String bookLink ="http://202.117.255.187:8080/opac/" + aTag.attr("href");
+                        String bookIntroduce = pTag.text().substring(0,pTag.text().length()-6);
+                        String bookNameReal = aTag.text().substring(aTag.text().indexOf(".") + 1);
+                        System.out.println("书名：" + bookName);
+                        System.out.println("链接：" + bookLink);
+                        System.out.println("介绍：" + bookIntroduce);
+                        System.out.println();
+                        Map<String, String> map = new HashMap<String, String>();
+
+                        map.put("bookName", bookName);
+                        map.put("bookLink", bookLink);
+                        map.put("bookIntroduce", bookIntroduce);
+                        map.put("bookNameReal", bookNameReal);
+                        list.add(map);
+                    }
+                    return list;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            @Override
+            protected void onPostExecute(List<Map<String, String>> list) {
+                progressBar.setVisibility(View.GONE);
+                if (list == null){
+                    Toast.makeText(MainActivity.this, "无结果", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                listView.setAdapter(new SimpleAdapter(MainActivity.this, list, android.R.layout.simple_list_item_2,
+                        new String[] {"bookName","bookIntroduce"}, new int[] {android.R.id.text1, android.R.id.text2}));
+                super.onPostExecute(list);
+            }
+
+        }.execute(book,page);
     }
 }
