7af6b4888ec5c6214d38f34c280b260ca2b8d33d
rty813 <523213189@qq.com>
2016-12-01 17:37:10 +0800

    添加了Jsoup-1.6.1

--- app/src/main/java/com/npu/zhang/npulibrary/MainActivity.java
+++ app/src/main/java/com/npu/zhang/npulibrary/MainActivity.java
@@ -9,8 +9,10 @@ import android.support.v7.widget.ScrollingTabContainerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,11 +31,16 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private EditText editText;
     private TextView textView;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         progressBar.setVisibility(View.GONE);
         editText = (EditText) findViewById(R.id.editText2);
         textView = (TextView) findViewById(R.id.textView2);
+        listView = (ListView) findViewById(R.id.listView);
 
         findViewById(R.id.btnSearch).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,34 +57,39 @@ public class MainActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.VISIBLE);
                 String book = editText.getText().toString();
 
-                new AsyncTask<String, Void, String>(){
+                new AsyncTask<String, Void, List<Map<String, String>>>(){
                     @Override
-                    protected String doInBackground(String... strings) {
+                    protected List<Map<String, String>> doInBackground(String... strings) {
                         try {
                             System.out.println("Begin to connnet!");
                             Document document = Jsoup.parse(new URL("http://202.117.255.187:8080/opac/openlink.php?strSearchType=title&strText=" + strings[0] + "&page=1"), 5000);
                             System.out.println("Connect successful!");
                             Element link = document.select("ol").first();
                             Elements atags = link.select("a");
-                            StringBuilder builder = new StringBuilder();
-                            for (Element atag : atags){
-                                if (atag.text().equals("馆藏"))
+                            Elements h3Tags = link.select("h3");
+                            List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+                            for (Element h3Tag : h3Tags){
+                                if (h3Tag.text().equals("馆藏"))
                                     continue;
-                                System.out.println("书名：" + atag.text());
-                                System.out.println("    链接：" + atag.attr("href"));
-                                builder.append(atag.text() + "\n");
+                                System.out.println("书名：" + h3Tag.text().substring(4));
+                                System.out.println("    链接：" + h3Tag.select("a").attr("href"));
+                                Map<String, String> map = new HashMap<String, String>();
+                                map.put("Name", h3Tag.text().substring(4));
+                                map.put("href","http://202.117.255.187:8080/opac/" + h3Tag.select("a").attr("href"));
+                                list.add(map);
                             }
-                            return builder.toString();
+                            return list;
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
                         return null;
                     }
                     @Override
-                    protected void onPostExecute(String string) {
-                        textView.setText(string);
+                    protected void onPostExecute(List<Map<String, String>> list) {
+                        listView.setAdapter(new SimpleAdapter(MainActivity.this, list, android.R.layout.simple_list_item_2,
+                                new String[] {"Name","href"}, new int[] {android.R.id.text1, android.R.id.text2}));
                         progressBar.setVisibility(View.GONE);
-                        super.onPostExecute(string);
+                        super.onPostExecute(list);
                     }
                 }.execute(book);
             }
